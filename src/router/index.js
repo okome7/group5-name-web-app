599e728b6540fc4530d98d6249044a94f990e1e7
大井　千絵子 <2420716@mwu.jp>
2026-07-29 11:19:11 +0900

    PRテスト用のコメント追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+import TopView from '../views/TopView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    { path: '/', name: 'login', component: LoginView },
+    { path: '/top', name: 'top', component: TopView }
+  ]
+});
 
-export default router
+export default router;
